3099c8fda090cc44944b4719fad4c2b2003cb67b
adi518 <adi.s@openweb.com>
2021-09-10 15:48:17 +0300

    fix script; upgrade deps

--- index.js
+++ index.js
@@ -1,51 +1,54 @@
-const fs = require('fs')
-const dotenv = require('dotenv')
-const puppeteer = require('puppeteer')
+const fs = require("fs");
+const dotenv = require("dotenv");
+const puppeteer = require("puppeteer");
 
 function overrideEnv() {
-  const envConfig = dotenv.parse(fs.readFileSync('.env'))
+  const envConfig = dotenv.parse(fs.readFileSync(".env"));
   for (const k in envConfig) {
-    process.env[k] = envConfig[k]
+    process.env[k] = envConfig[k];
   }
 }
 
-overrideEnv()
+overrideEnv();
 
 const WISHLIST_URL =
-  'https://my.aliexpress.com/wishlist/wish_list_product_list.htm'
+  "https://my.aliexpress.com/wishlist/wish_list_product_list.htm";
 
-;(async ({
-  username,
-  password,
-  usernameSelector,
-  passwordSelector,
-  submitSelector,
-  removeSelector
-}) => {
+(async ({ username, password, usernameSelector, removeItemSelector }) => {
   const browser = await puppeteer.launch({
     headless: false,
     defaultViewport: null,
-    args: ['--start-maximized']
-  })
-  const pages = await browser.pages()
-  const page = pages[0]
-  await page.goto(WISHLIST_URL)
-  await page.waitForSelector(usernameSelector, { visible: true })
-  await page.focus(usernameSelector)
-  await page.keyboard.type(username)
-  await page.waitForSelector(passwordSelector, { visible: true })
-  await page.focus(passwordSelector)
-  await page.keyboard.type(password)
-  await Promise.all([page.waitForNavigation(), page.click(submitSelector)])
-  while ((await page.$$(removeSelector)).length) {
-    await Promise.all([page.waitForNavigation(), page.click(removeSelector)])
+    args: ["--start-maximized"],
+  });
+  const [page] = await browser.pages();
+
+  page.setDefaultNavigationTimeout(0);
+
+  async function login() {
+    await page.waitForSelector(usernameSelector, { visible: true });
+    await page.focus(usernameSelector);
+    await page.keyboard.type(username);
+    await page.waitForSelector("[type=password]", { visible: true });
+    await page.focus("[type=password]");
+    await page.keyboard.type(password);
+    await page.click("[type=submit]");
+    await page.waitForNavigation();
   }
-  await browser.close()
+
+  await page.goto(WISHLIST_URL);
+  await login();
+
+  while ((await page.$$(removeItemSelector)).length) {
+    await Promise.all([
+      page.waitForNavigation(),
+      page.click(removeItemSelector),
+    ]);
+  }
+
+  await browser.close();
 })({
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
-  usernameSelector: '#fm-login-id',
-  passwordSelector: '#fm-login-password',
-  submitSelector: 'button[type=submit]',
-  removeSelector: '.remove'
-})
+  removeItemSelector: ".remove",
+  usernameSelector: "#fm-login-id",
+});
